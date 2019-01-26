080fe28aa20d4994d41d24187b4bc169f722f475
Aaron Stannard <aaron@petabridge.com>
2019-01-26 04:16:48 -0600

    added a specification and a default HOCON file

---
+++ src/Akka.HealthCheck.Tests/Configuration/DefaultHealthCheckSettingsSpecs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Configuration;
+using Akka.HealthCheck.Configuration;
+using FluentAssertions;
+using Xunit;
+
+namespace Akka.HealthCheck.Tests.Configuration
+{
+    public class DefaultHealthCheckSettingsSpecs : TestKit.Xunit.TestKit
+    {
+        [Fact(DisplayName = "Should be able to load default Akka.HealthCheck HOCON")]
+        public void Should_load_default_HealthCheck_HOCON()
+        {
+            HealthCheckSettings.DefaultConfig().Should().NotBe(Config.Empty);
+        }
+    }
+}
---
+++ src/Akka.HealthCheck/Configuration/HealthCheckSettings.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Configuration;
+
+namespace Akka.HealthCheck.Configuration
+{
+   /// <summary>
+   /// All of the settings used by the Akka.HealthCheck extension.
+   /// </summary>
+    public sealed class HealthCheckSettings
+    {
+        /// <summary>
+        /// Used to load the default HOCON <see cref="Config"/> used by Akka.HealthCheck
+        /// at startup.
+        /// </summary>
+        public static Config DefaultConfig()
+        {
+            return ConfigurationFactory.FromResource<HealthCheckSettings>("Akka.HealthCheck.Configuration.akka.healthcheck.conf");
+        }
+    }
+}
