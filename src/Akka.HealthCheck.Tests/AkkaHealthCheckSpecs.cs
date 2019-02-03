d7366a36b51540825c8c09794c66de4b13a4036b
Aaron Stannard <aaron@petabridge.com>
2019-02-02 18:47:48 -0600

    added spec to validate that plugin can be loaded by default

---
+++ src/Akka.HealthCheck.Tests/AkkaHealthCheckSpecs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.HealthCheck.Liveness;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.HealthCheck.Tests
+{
+    public class AkkaHealthCheckSpecs 
+    {
+        [Fact(DisplayName = "Should load default AkkaHealthCheck plugin and settings")]
+        public async Task Should_load_default_AkkaHealthCheck()
+        {
+            using (var system = ActorSystem.Create("foo"))
+            {
+                var healthCheck = AkkaHealthCheck.For(system);
+                healthCheck.Settings.Misconfigured.Should().BeFalse();
+                var status = await healthCheck.LivenessProbe.Ask<LivenessStatus>(GetCurrentLiveness.Instance, TimeSpan.FromSeconds(1));
+                status.IsLive.Should().BeTrue();
+            }
+        }
+    }
+}
