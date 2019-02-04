493bb76e34462ad9df175b78dc0c3606d484baa5
Aaron Stannard <aaron@petabridge.com>
2019-02-04 10:46:29 -0600

    Added timeout spec

--- src/Akka.HealthCheck.Tests/Liveness/LivenessTransportActorSpecs.cs
+++ src/Akka.HealthCheck.Tests/Liveness/LivenessTransportActorSpecs.cs
@@ -52,5 +52,27 @@ namespace Akka.HealthCheck.Tests.Liveness
             AwaitCondition(() => testTransport.SystemCalls.Count == 1
                                  && testTransport.SystemCalls[0] == TestStatusTransport.TransportCall.Stop);
         }
+
+
+        [Fact(DisplayName = "LivenessTransportActor should crash and try to stop signal upon timeout during signal change")]
+        public void LivenessTransportActor_should_crash_when_Timedout()
+        {
+            var testTransport = new TestStatusTransport(new TestStatusTransportSettings(true, true, TimeSpan.FromSeconds(1.5)));
+            var fakeLiveness = CreateTestProbe("liveness");
+
+            var transportActor =
+                Sys.ActorOf(Props.Create(() => new LivenessTransportActor(testTransport, fakeLiveness)));
+
+            fakeLiveness.ExpectMsg<SubscribeToLiveness>();
+            fakeLiveness.Reply(new LivenessStatus(true));
+
+            AwaitCondition(() => testTransport.SystemCalls.Count == 1
+                                 && testTransport.SystemCalls[0] == TestStatusTransport.TransportCall.Go);
+
+            fakeLiveness.ExpectMsg<SubscribeToLiveness>();
+            fakeLiveness.Reply(new LivenessStatus(false));
+            AwaitCondition(() => testTransport.SystemCalls.Count == 2
+                                 && testTransport.SystemCalls[1] == TestStatusTransport.TransportCall.Stop);
+        }
     }
 }
