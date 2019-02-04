7c5749f4d8956688accca012b25290155da4ceac
Aaron Stannard <aaron@petabridge.com>
2019-02-04 14:50:15 -0600

    Cluster healthcheck algos (#7)
    * implement clustering liveness / readiness strategies
    
    * added cluster liveness check implementation
    
    * finished cluster readiness probe
    
    * added current v0.1.0 release notes

---
+++ src/Akka.Cluster.HealthCheck/ClusterLivenessProbeProvider.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ClusterLivenessProbeProvider.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+using Akka.HealthCheck;
+
+namespace Akka.Cluster.HealthCheck
+{
+    /// <summary>
+    ///     <see cref="IProbeProvider" /> liveness implementation intended for use with Akka.Cluster.
+    /// </summary>
+    public sealed class ClusterLivenessProbeProvider : ProbeProviderBase
+    {
+        public ClusterLivenessProbeProvider(ActorSystem system) : base(system)
+        {
+        }
+
+        public override Props ProbeProps => Props.Create(() => new ClusterLivenessProbe());
+    }
+}
