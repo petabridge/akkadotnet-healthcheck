2e44345887a7310f7cc770ae383ef805f0f961b1
Aaron Stannard <aaron@petabridge.com>
2019-02-04 14:22:35 -0600

    implement clustering liveness / readiness strategies

---
+++ src/Akka.Cluster.HealthCheck/ClusterLivenessProbeProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Akka.Actor;
+using Akka.HealthCheck;
+
+namespace Akka.Cluster.HealthCheck
+{
+    /// <summary>
+    /// <see cref="IProbeProvider"/> implementation intended for use with Akka.Cluster.
+    /// </summary>
+    public sealed class ClusterLivenessProbeProvider : ProbeProviderBase
+    {
+        public ClusterLivenessProbeProvider(ActorSystem system) : base(system)
+        {
+        }
+
+        public override Props ProbeProps { get; }
+    }
+
+
+}
