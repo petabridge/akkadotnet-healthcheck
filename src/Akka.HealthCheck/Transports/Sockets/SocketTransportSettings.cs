0cc8fc50051e0d7eb871a4d52514b94404d8a204
Aaron Stannard <aaron@petabridge.com>
2019-02-04 11:35:33 -0600

    implemented SocketStatusTransport

---
+++ src/Akka.HealthCheck/Transports/Sockets/SocketTransportSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Akka.HealthCheck.Transports.Sockets
+{
+    /// <summary>
+    /// Settings class for the <see cref="SocketStatusTransport"/>
+    /// </summary>
+    public sealed class SocketTransportSettings : ITransportSettings
+    {
+        public SocketTransportSettings(int port)
+        {
+            Port = port;
+        }
+
+        /// <summary>
+        /// The port used to open the TCP socket.
+        /// </summary>
+        public int Port { get; }
+
+        public ProbeTransport TransportType => ProbeTransport.TcpSocket;
+        public string StartupMessage => $"Writing probe data to TCP Socket at [0.0.0.0]{Port}";
+    }
+}
