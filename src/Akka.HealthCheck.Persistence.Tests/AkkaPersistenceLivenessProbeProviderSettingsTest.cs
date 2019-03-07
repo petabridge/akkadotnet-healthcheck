969fb8db663581776b3b1c53646e3161d2491207
izavala <isaac.zavala@petabridge.com>
2019-03-07 16:15:46 -0600

    Updated File name on header

--- src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
+++ src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="UnitTest1.cs" company="Petabridge, LLC">
+// <copyright file="AkkaPersistenceLivenessProbeProviderSettingsTestAkkaPersistenceLivenessProbeProviderSettingsTest.cs" company="Petabridge, LLC">
 //      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
 // </copyright>
 // -----------------------------------------------------------------------
