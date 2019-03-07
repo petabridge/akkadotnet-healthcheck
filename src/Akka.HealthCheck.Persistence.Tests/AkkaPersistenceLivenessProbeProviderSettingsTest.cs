55b755712733103d4aa17c0478bdf9ae2e468032
izavala <isaac.zavala@petabridge.com>
2019-03-07 16:16:26 -0600

    Fixed header being buplicated

--- src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
+++ src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
@@ -1,5 +1,5 @@
 // -----------------------------------------------------------------------
-// <copyright file="AkkaPersistenceLivenessProbeProviderSettingsTestAkkaPersistenceLivenessProbeProviderSettingsTest.cs" company="Petabridge, LLC">
+// <copyright file="AkkaPersistenceLivenessProbeProviderSettingsTest.cs" company="Petabridge, LLC">
 //      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
 // </copyright>
 // -----------------------------------------------------------------------
