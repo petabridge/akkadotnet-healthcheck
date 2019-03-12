fba382cce3268061332cd3095bee2df1a56ba610
izavala <isaac.zavala@petabridge.com>
2019-03-12 14:27:16 -0500

    Added test for snapshot store misconfiguration

--- src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeNotAvailableDueToSnapshotStoreSpecs.cs
+++ src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeNotAvailableDueToSnapshotStoreSpecs.cs
@@ -1,8 +1,9 @@
 ﻿// -----------------------------------------------------------------------
-// <copyright file="AkkaPersistenceLivenessProbeNotAvailableduetojurnalSpecs.cs" company="Petabridge, LLC">
+// <copyright file="AkkaPersistenceLivenessProbeNotAvailableDueToSnapshotStoreSpecs.cs" company="Petabridge, LLC">
 //      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
 // </copyright>
 // -----------------------------------------------------------------------
+
 using Akka.Actor;
 using Akka.HealthCheck.Liveness;
 using FluentAssertions;
@@ -14,35 +15,35 @@ using static Akka.HealthCheck.Persistence.AkkaPersistenceLivenessProbe;
 
 namespace Akka.HealthCheck.Persistence.Tests
 {
-    public class AkkaPersistenceLivenessProbeNotAvailableDueToJurnalSpecs : TestKit.Xunit.TestKit
+    public class AkkaPersistenceLivenessProbeNotAvailableDueToSnapshotStoreSpecs : TestKit.Xunit.TestKit
     {
-        public AkkaPersistenceLivenessProbeNotAvailableDueToJurnalSpecs(ITestOutputHelper helper)
+        public AkkaPersistenceLivenessProbeNotAvailableDueToSnapshotStoreSpecs(ITestOutputHelper helper)
                     : base(config, output: helper)
         {
         }
         public static string config = @"akka.persistence {
-                                         journal {
+                                         journal { 
                                                     plugin = ""akka.persistence.journal.sqlite""
                                                     recovery-event-timeout = 2s
                                                     circuit-breaker.reset-timeout = 2s
                                                     sqlite {
                                                             class = ""Akka.Persistence.Sqlite.Journal.SqliteJournal, Akka.Persistence.Sqlite""
                                                             auto-initialize = on
-                                                            connection-string = ""Fake=file:memdb.db;Mode=Memory;Cache=Shared"" #Invalid connetion string             
+                                                            connection-string = ""Filename=file:memdb.db;Mode=Memory;Cache=Shared""                  
                                                      }}
                                          snapshot-store {
                                                 plugin = ""akka.persistence.snapshot-store.sqlite""
                                                 sqlite {
                                                 class = ""Akka.Persistence.Sqlite.Snapshot.SqliteSnapshotStore, Akka.Persistence.Sqlite""
                                                 auto-initialize = on
-                                                connection-string = ""Filename=file:memdb.db;Mode=Memory;Cache=Shared""
-                       }
-                   }}";
+                                                connection-string = ""Filename=file:memdb.db;Mode=Memory;Cache=Shared"" #Invalid connetion string
+                                                     }
+                                          }}";
 
-        [Fact(DisplayName = " ActorSystem should correcly report when Akk.Persistence is unavailable due to bad journal configuration")]
-        public void AkkaPersistenceLivenessProbeProvidert_Should_Report_Akka_Persistance_Is_Unavailable_With_Bad_Journal_Setup()
+        [Fact(DisplayName = " ActorSystem should correcly report when Akk.Persistence is unavailable due to bad snapshot-store configuration")]
+        public void AkkaPersistenceLivenessProbeProvidert_Should_Report_Akka_Persistance_Is_Unavailable_With_Bad_Snapshot_Store_Setup()
         {
-            
+
             var ProbActor = Sys.ActorOf(Props.Create(() => new AkkaPersistenceLivenessProbe(TimeSpan.FromMilliseconds(250))));
             ProbActor.Tell(new SubscribeToLiveness(TestActor));
             ExpectMsg<LivenessStatus>().IsLive.Should().BeFalse("System should not be live");
