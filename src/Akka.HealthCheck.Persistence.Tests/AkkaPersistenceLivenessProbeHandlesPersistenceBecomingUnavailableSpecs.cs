97be624d62ffb4791bc5f77197c176369e4a663a
izavala <isaac.zavala@petabridge.com>
2019-03-12 19:03:10 -0500

    Added will become unavlailable test

---
+++ src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeHandlesPersistenceBecomingUnavailableSpecs.cs
@@ -0,0 +1,93 @@
+﻿using Akka.Actor;
+using Akka.HealthCheck.Liveness;
+using Akka.Persistence;
+using Akka.Persistence.Journal;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.HealthCheck.Persistence.Tests
+{
+    public class AkkaPersistenceLivenessProbeHandlesPersistenceBecomingUnavailableSpecs : TestKit.Xunit.TestKit
+    {
+        public AkkaPersistenceLivenessProbeHandlesPersistenceBecomingUnavailableSpecs(ITestOutputHelper helper)
+                    : base(TestConfig.GetCustomConfigurationString, output: helper)
+        {
+        }
+
+        internal class Evt
+        {
+            public Evt(object data)
+            {
+                Data = data;
+            }
+
+            public object Data { get; private set; }
+
+            public override string ToString()
+            {
+                return "Evt(" + Data + ")";
+            }
+        }
+        internal class SimulatedException : Exception
+        {
+            public SimulatedException()
+            {
+            }
+
+            public SimulatedException(string message) : base(message)
+            {
+            }
+        }
+        internal class DoomedMemoryJournal : MemoryJournal
+        {
+            protected override Task<IImmutableList<Exception>> WriteMessagesAsync(IEnumerable<AtomicWrite> messages)
+            {
+                var msgs = messages.ToList();
+                if (KillJournal(msgs))
+                    throw new SimulatedException("Simulated Store failure");
+
+                foreach (var w in messages)
+                {
+                    foreach (var p in (IEnumerable<IPersistentRepresentation>)w.Payload)
+                    {
+                       
+                            Add(p);
+                    }
+                }
+                return Task.FromResult((IImmutableList<Exception>)null); // all good
+            }
+
+            private bool KillJournal(IEnumerable<AtomicWrite> messages)
+            {
+                return
+                    messages.Any(
+                        a =>
+                            ((IEnumerable<IPersistentRepresentation>)a.Payload).Any(
+                                p => ((string)((Evt)p.Payload).Data).Contains("wrong")));
+            }
+        }
+
+        [Fact(DisplayName = " ActorSystem should correcly report when Akk.Persistence is unavailable due to bad snapshot-store configuration")]
+        public void AkkaPersistenceLivenessProbeProvidert_Should_Report_Akka_Persistance_Become_Unavailable()
+        {
+
+            var ProbActor = Sys.ActorOf(Props.Create(() => new AkkaPersistenceLivenessProbe(TimeSpan.FromMilliseconds(250))));
+            ProbActor.Tell(new SubscribeToLiveness(TestActor));
+            ExpectMsg<LivenessStatus>().IsLive.Should().BeFalse("System should not be live");
+            ExpectMsg<LivenessStatus>(TimeSpan.FromMinutes(1)).IsLive.Should().BeFalse("System should not be live");
+
+        }
+    }
+
+
+    
+
+
+
+}
