d1b0a1905a295cddd648f5fe4901cf4fec922b76
izavala <isaac.zavala@petabridge.com>
2019-03-14 09:06:51 -0500

    Cluster formation

---
+++ src/Akka.Cluster.HealthCheck.Tests/ReadinessStatusClusterUpSpecs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Akka.Cluster.HealthCheck.Tests
+{
+    class ReadinessStatusClusterUpSpecs : TestKit.Xunit.TestKit
+    {
+
+
+        [Fact(DisplayName = "ReadinessStatsuCluster should tell subscribers that it is up once it becomes avaialable")]
+        public void ReadinessStatusCluster_Sould_Tell_Subsribers_When_It_Becomes_Avaialabe()
+        {
+
+        }
+         
+    }
+
+    
+}
