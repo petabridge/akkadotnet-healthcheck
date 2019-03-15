e4f3e06e28ecf40e6a7591252cad44b63052cb8d
Aaron Stannard <aaron@petabridge.com>
2019-03-14 19:16:36 -0500

    forgot to add stuff

---
+++ src/Akka.HealthCheck.Cluster.Tests/ReadinessStatusClusterUpSpecs.cs
@@ -0,0 +1,76 @@
+﻿using Akka.Actor;
+using Akka.HealthCheck.Readiness;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Akka.HealthCheck.Cluster.Tests
+{
+    public class ReadinessStatusClusterUpSpecs : TestKit.Xunit.TestKit
+    {
+        public ReadinessStatusClusterUpSpecs(ITestOutputHelper helper)
+                    : base(confi, "ClusterServer", output: helper)
+        {
+        }
+        private const string confi = @"
+        akka {   
+   
+    actor {
+       
+        provider = cluster
+}
+    remote {
+        dot-netty.tcp {
+            hostname = ""127.0.0.1""
+            port = 3000
+        }
+    }
+    cluster {
+        seed-nodes = [""akka.tcp://ClusterServer@127.0.0.1:3000""]
+    }  
+}
+";
+
+        public Akka.Cluster.Cluster Cluster => Akka.Cluster.Cluster.Get(Sys);
+
+
+        [Fact(DisplayName = "ReadinessStatsuCluster should tell subscribers that it is up once it becomes avaialable")]
+        public void ReadinessStatusCluster_Sould_Tell_Subsribers_When_It_Becomes_Avaialabe()
+        {
+            // step1 - verify joining of cluster
+            Cluster.RegisterOnMemberUp(() => TestActor.Tell("ready"));
+            ExpectMsg("ready");
+
+            // step2 - create probe
+
+            var probe = Sys.ActorOf(Props.Create(() => new ClusterReadinessProbe()));
+            probe.Tell(new SubscribeToReadiness(TestActor));
+
+            // step3 - wait for ready status
+            FishForMessage<ReadinessStatus>(r => r.IsReady);
+        }
+
+        [Fact(DisplayName = "ReadinessStatsuCluster should tell subscribers when it leaves the Cluster")]
+        public void ReadinessStatusCluster_Sould_Tell_Subsribers_When_It_Leaves_Cluster()
+        {
+            // step1 - verify joining of cluster
+            Cluster.RegisterOnMemberUp(() => TestActor.Tell("ready"));
+            ExpectMsg("ready");
+
+            // step2 - create probe
+
+            var probe = Sys.ActorOf(Props.Create(() => new ClusterReadinessProbe()));
+            probe.Tell(new SubscribeToReadiness(TestActor));
+
+            // step3 - wait for ready status
+            FishForMessage<ReadinessStatus>(r => r.IsReady);
+
+            Cluster.Leave(Cluster.SelfAddress);
+            FishForMessage<ReadinessStatus>(r => !r.IsReady);
+
+
+        }
+
+    }
+
+
+}
---
+++ src/Akka.HealthCheck.Cluster/ClusterLivenessProbeProvider.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ClusterLivenessProbeProvider.cs" company="Petabridge, LLC">
+//      Copyright (C) 2015 - 2019 Petabridge, LLC <https://petabridge.com>
+// </copyright>
+// -----------------------------------------------------------------------
+
+using Akka.Actor;
+
+namespace Akka.HealthCheck.Cluster
+{
+    /// <summary>
+    ///     <see cref="IProbeProvider" /> liveness implementation intended for use with Akka.Cluster.
+    /// </summary>
+    public sealed class ClusterLivenessProbeProvider : ProbeProviderBase
+    {
+        public ClusterLivenessProbeProvider(ActorSystem system) : base(system)
+        {
+        }
+
+        public override Props ProbeProps => Props.Create(() => new ClusterLivenessProbe());
+    }
+}
