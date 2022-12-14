5f903e6b5fe9e9ee86a93a1f1722d4aa3ef1f4d8
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-12-15 03:38:15 +0700

    Simplify persistence settings test

--- src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
+++ src/Akka.HealthCheck.Persistence.Tests/AkkaPersistenceLivenessProbeProviderSettingsTest.cs
@@ -5,35 +5,45 @@
 // -----------------------------------------------------------------------
 using Akka.Actor;
 using Akka.Configuration;
+using Akka.HealthCheck.Configuration;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
 
 namespace Akka.HealthCheck.Persistence.Tests
 {
-    public class AkkaPersistenceLivenessProbeProviderSettingsTest : TestKit.Xunit.TestKit
+    public class AkkaPersistenceLivenessProbeProviderSettingsTest
     {
-
-        public AkkaPersistenceLivenessProbeProviderSettingsTest(ITestOutputHelper helper)
-            : base(HoconString, output: helper)
+        [Fact(DisplayName = " ActorSystem should correcly load AkkaPersistenceLivenessProbeProvider from HOCON configuration")]
+        public void AkkaPersistenceLivenessProbeProviderSettingsTest_Should_Load()
         {
-        }
-        public static string HoconString = @"
+            var config = ConfigurationFactory.ParseString(@"
                     akka.healthcheck{
                         liveness {
                             providers {
                                 default = ""Akka.HealthCheck.Persistence.AkkaPersistenceLivenessProbeProvider, Akka.HealthCheck.Persistence""
                             }
                         }
-                    }";
+                    }").WithFallback(HealthCheckSettings.DefaultConfig());
 
-
-        [Fact(DisplayName = " ActorSystem should correcly load AkkaPersistenceLivenessProbeProvider from HOCON configuration")]
-        public void AkkaPersistenceLivenessProbeProviderSettingsTest_Should_Load()
+            var settings = new HealthCheckSettings(config.GetConfig("akka.healthcheck"));
+            settings.Misconfigured.Should().BeFalse();
+            settings.LivenessProbeProvider.Should().Be(typeof(AkkaPersistenceLivenessProbeProvider));
+        }
+        
+        [Fact(DisplayName = " ActorSystem should correcly load AkkaPersistenceLivenessProbeProvider from HOCON configuration (compat)")]
+        public void AkkaPersistenceLivenessProbeProviderSettingsTest_Should_Load_compat()
         {
-            var healthCheck = AkkaHealthCheck.For(Sys);
-            healthCheck.Settings.Misconfigured.Should().BeFalse();
-            healthCheck.Settings.LivenessProbeProvider.Should().Be(typeof(AkkaPersistenceLivenessProbeProvider));
+            var config = ConfigurationFactory.ParseString(@"
+                    akka.healthcheck{
+                        liveness {
+                            provider = ""Akka.HealthCheck.Persistence.AkkaPersistenceLivenessProbeProvider, Akka.HealthCheck.Persistence""
+                        }
+                    }").WithFallback(HealthCheckSettings.DefaultConfig());
+
+            var settings = new HealthCheckSettings(config.GetConfig("akka.healthcheck"));
+            settings.Misconfigured.Should().BeFalse();
+            settings.LivenessProbeProvider.Should().Be(typeof(AkkaPersistenceLivenessProbeProvider));
         }
     }
 }
